de5ef0db997d3f790867aaa2f0e3fa7f65a7099c
bpben <B@Bens-MacBook-Pro.local>
2016-11-21 10:59:03 -0500

    add global package install

---
+++ global.R
@@ -0,0 +1,11 @@
+#Load/install required packages
+testin <- function(package){
+  if (!package %in% installed.packages())
+    install.packages(package) }
+testin("markdown")
+testin("shiny")
+testin("shinydashboard")
+testin('shiny')
+testin('reshape2')
+testin('ggplot2')
+testin('RColorBrewer')
